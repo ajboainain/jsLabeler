e420bc7d177c4174dffab0de61b57e0f1f2d08d1
aalboainain <aalboainain@barzanholdings.com>
2022-05-23 15:14:13 +0300

    mo tings

--- jsLabeler.js
+++ jsLabeler.js
@@ -17,15 +17,15 @@ boxes.forEach(box => {
 //   }
 // });
 
-function setCircleAttributes(circle, cx, cy, r) {
+function setCircleAttributes(circle, cx, cy, r, fill) {
   circle.setAttributeNS(null, 'cx', cx);
   circle.setAttributeNS(null, 'cy', cy);
   circle.setAttributeNS(null, 'r', r);
-  circle.setAttributeNS(null, 'fill', "rgba(15, 252, 3, 0.8)");
+  circle.setAttributeNS(null, 'fill', fill);
   circle.setAttributeNS(null, 'stroke', "black");
   circle.setAttributeNS(null, 'stroke-opacity', "0.8");
   circle.setAttributeNS(null, 'stroke-width', "1");
-  circle.setAttributeNS(null, 'fill-opacity', "0.1");
+  circle.setAttributeNS(null, 'fill-opacity', "1");
   return;
 }
 
@@ -40,6 +40,7 @@ function setRectAttributes(rect, x, y, w, h) {
   rect.setAttribute('stroke-width', "2");
   rect.setAttribute('fill-opacity', "0.1");
 }
+//rgba(15, 252, 3, 0.8)
 
 const svg = document.querySelector('#svg');
 const svgPoint = (elem, x, y) => {
@@ -50,8 +51,6 @@ const svgPoint = (elem, x, y) => {
 };
 //https://javascript.info/mouse-drag-and-drop
 svg.onmousedown = function (event) {
-  // ('mousedown', (event) => {
-
   var moving = false;
   
   
@@ -62,12 +61,34 @@ svg.onmousedown = function (event) {
 
     let shiftX = event.clientX - currentRect.getBoundingClientRect().left;
     let shiftY = event.clientY - currentRect.getBoundingClientRect().top ;
+    let shiftBottom = event.clientY - currentRect.getBoundingClientRect().bottom;
+    let shiftRight = event.clientX - currentRect.getBoundingClientRect().right;
 
     currentRect.style.position = 'absolute';
     currentRect.style.zIndex = 1000;
     currentGrouping.append(currentRect);
 
     function moveAt(pageX, pageY) {
+      const coords = svgPoint(svg, event.clientX, event.clientY);
+      var circles = function () {
+        var temp = []
+        currentGrouping.childNodes.forEach(child =>{
+          if (child.tagName == "circle") 
+            temp.push(child);
+        });
+        return temp;
+      }
+      var circlesList = circles();
+      if (circlesList.length > 0) {
+        setCircleAttributes(circlesList[0], pageX - shiftX, pageY - shiftY, 3, "black");
+        setCircleAttributes(circlesList[1], pageX - shiftX, pageY - shiftBottom - 8, 3, "white");
+        setCircleAttributes(circlesList[2], pageX - shiftRight - 20, pageY - shiftY, 3, "orange");
+        setCircleAttributes(circlesList[3], pageX - shiftRight - 20, pageY - shiftBottom - 8, 3, "blue");
+
+      }
+      // setCircleAttributes(circlesList[2], pageX - shiftX, pageY - shiftY, 3);
+      // setCircleAttributes(circlesList[3], pageX - shiftX, pageY - shiftY, 3);
+      
       currentRect.setAttributeNS(null, 'x', pageX - shiftX);
       currentRect.setAttributeNS(null, 'y', pageY - shiftY);
     }
@@ -84,8 +105,6 @@ svg.onmousedown = function (event) {
       currentRect.onmouseup = null;
       moving = false;
     };
-
-    // console.log(shiftX);
     return;
   }
 
@@ -115,10 +134,10 @@ svg.onmousedown = function (event) {
     }
 
     
-    setCircleAttributes(circleTL, p.x, p.y, 3);
-    setCircleAttributes(circleTR, p.x, p.y+h, 3);
-    setCircleAttributes(circleBL, p.x+w, p.y, 3);
-    setCircleAttributes(circleBR, p.x+w, p.y+h, 3);
+    setCircleAttributes(circleTL, p.x, p.y, 3, "black");
+    setCircleAttributes(circleTR, p.x, p.y+h, 3, "white");
+    setCircleAttributes(circleBL, p.x+w, p.y, 3, "orange");
+    setCircleAttributes(circleBR, p.x+w, p.y+h, 3, "blue");
 
     setRectAttributes(rect, p.x, p.y, w, h);
 
@@ -135,11 +154,16 @@ svg.onmousedown = function (event) {
   const endDraw = (e) => {
     svg.removeEventListener('mousemove', drawRect);
     svg.removeEventListener('mouseup', endDraw);
-    // console.log(circleTL);
-    // console.log(grouping);
   };
   
   svg.addEventListener('mousemove', drawRect);
   svg.addEventListener('mouseup', endDraw);
 };
 
+function reportWindowSize() {
+  // put code to change the width and height attributes of svg for better rect moving
+  console.log(window.innerHeight);
+  console.log(window.innerWidth);
+}
+
+window.onresize = reportWindowSize;
